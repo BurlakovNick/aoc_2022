42af8c12e16f8f0c5617f0f38b7591d00a7d15dd
Nick Burlakov <nick@istyping.cc>
2022-12-01 21:58:49 +0400

    Fix + day 2 prep

--- 01.py
+++ 01.py
@@ -4,4 +4,4 @@ blocks = read_blocks(parse=parse_value)
 sums = sorted(map(sum, blocks), reverse=True)
 
 print("Part One", sums[0])
-print("Part Two", sums[0] + sums[1] + sums[2])
+print("Part Two", sum(sums[:3]))
