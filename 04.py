e039c12eefb6d48c71382a9132d5330047e23c22
Nick Burlakov <nick@istyping.cc>
2022-12-04 09:28:50 +0400

    fix

--- 04.py
+++ 04.py
@@ -7,5 +7,5 @@ for line in inp:
     lx, rx, ly, ry = [int(x) for x in re.split(",|-", line)]
     sets.append([set(range(lx, rx + 1)), set(range(ly, ry + 1))])
 
-print("Part One", len([(x, y) for x, y in sets if x.issuperset(y) or y.issuperset(x)]))
-print("Part Two", len([(x, y) for x, y in sets if x & y]))
+print("Part One", sum(1 for x, y in sets if x.issuperset(y) or y.issuperset(x)))
+print("Part Two", sum(1 for x, y in sets if x & y))
