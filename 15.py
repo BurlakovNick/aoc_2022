f66878767ca84a3e18d7e970631a5faca980e3db
Nick Burlakov <nick@istyping.cc>
2022-12-15 10:53:54 +0400

    fix

--- 15.py
+++ 15.py
@@ -13,23 +13,20 @@ def get_segments(y: int) -> list[list[int]]:
         if d < max_d:
             left = max_d - d
             results.append([s.x - left, s.x + left])
-    return results
+    return sorted(results, key=lambda s: s[0])
 
 
 def solve_1():
     target_y = 2000000
-    segments = sorted(get_segments(target_y), key=lambda s: s[0])
+    segments = get_segments(target_y)
     total = 0
     max_x = -100000000000
     for lx, rx in segments:
         if rx <= max_x:
             continue
-        elif lx > max_x:
-            total += rx - lx + 1 - sum(1 for b in beacons if b.y == target_y and lx <= b.x <= rx)
-            max_x = rx
-        elif rx > max_x:
-            total += rx - max_x - sum(1 for b in beacons if b.y == target_y and max_x + 1 <= b.x <= rx)
-            max_x = rx
+        lx = max(lx, max_x + 1)
+        total += rx - lx + 1 - sum(1 for b in beacons if b.y == target_y and lx <= b.x <= rx)
+        max_x = rx
     return total
 
 
@@ -39,7 +36,7 @@ print("Part One", ans)
 
 def solve_2():
     for y in range(0, 4000000):
-        segments = sorted(get_segments(y), key=lambda s: s[0])
+        segments = get_segments(y)
         max_x = -1
         for i in range(len(segments) - 1):
             if segments[i][1] + 2 > max_x and segments[i][1] + 2 == segments[i + 1][0]:
