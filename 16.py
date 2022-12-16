f61ffd4bdc1d2edeb6c936e0b76c235d006783e8
Nick Burlakov <nick@istyping.cc>
2022-12-16 23:06:49 +0400

    Clean up (and slow down)

--- 16.py
+++ 16.py
@@ -1,6 +1,8 @@
+from collections import defaultdict
 from functools import cache
 from aoc import *
 
+INF = 10000000000000
 tunnels: dict[str, list[str]] = {}
 valves_with_rate: dict[str, int] = {}
 valves_with_rate_index: dict[str, int] = {}
@@ -44,30 +46,25 @@ def get_total_rate(mask: int) -> int:
     return total
 
 
+# slow af but idk
 @cache
-def calc(mask: int, pos: str, minutes: int, minutes_max: int) -> int:
-    if minutes > minutes_max:
-        return -1
+def calc(mask: int, pos: str, minutes: int) -> int:
+    if minutes == 1 and pos == "AA" and mask == 0:
+        return 0
+    if minutes <= 1:
+        return -INF
 
-    if minutes == minutes_max:
-        if pos == "AA" and mask == 0:
-            return 0
-        else:
-            return -1
-
-    best = -1
+    best = -INF
     delta = get_total_rate(mask)
 
-    if (x := calc(mask, pos, minutes + 1, minutes_max)) != -1:
-        best = max(best, delta + x)
+    best = max(best, delta + calc(mask, pos, minutes - 1))
 
     for new_pos in valves_with_rate.keys():
-        if (x := calc(mask, new_pos, minutes + dist[pos][new_pos], minutes_max)) != -1:
-            best = max(best, delta * dist[pos][new_pos] + x)
+        best = max(best, delta * dist[pos][new_pos] + calc(mask, new_pos, minutes - dist[pos][new_pos]))
 
-    if (index := valves_with_rate_index.get(pos)) is not None and (mask & (1 << index)):
-        if (x := calc(mask - (1 << index), pos, minutes + 1, minutes_max)) != -1:
-            best = max(best, delta + x)
+    index = valves_with_rate_index.get(pos)
+    if mask & (1 << index):
+        best = max(best, delta + calc(mask - (1 << index), pos, minutes - 1))
 
     return best
 
@@ -76,27 +73,19 @@ def solve():
     best = -1
     for msk in range(0, (1 << n)):
         for pos in valves_with_rate.keys():
-            best = max(best, calc(msk, pos, 1, 30))
+            best = max(best, calc(msk, pos, 30))
     return best
 
 
 def solve_2():
-    best = {}
+    best = defaultdict(lambda: -INF)
     for msk in range(0, (1 << n)):
-        best_x = -1
         for pos in valves_with_rate.keys():
-            x = calc(msk, pos, 1, 26)
-            if x >= 0:
-                best_x = max(x, best_x)
-        best[msk] = best_x
+            best[msk] = max(best[msk], calc(msk, pos, 26))
 
-    answer = -1
+    answer = -INF
     for msk in range(0, (1 << n)):
-        if best[msk] == -1:
-            continue
         for msk_2 in get_submasks(((1 << n) - 1) - msk):
-            if best[msk_2] == -1:
-                continue
             answer = max(answer, best[msk] + best[msk_2])
     return answer
 
